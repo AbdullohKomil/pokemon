a0eb12fdb15f65b8210678350157359557a915fe
AbdullohKomil <akomiljonov@icloud.com>
2022-12-14 21:42:35 +0500

    add scss

--- js/main.js
+++ js/main.js
@@ -1,29 +1,28 @@
-
 function MyFunction(pokemons) {
-  for (Object of pokemons) {
+  for (i of pokemons) {
     var clRow = document.querySelector(".row");
     var createColBox = document.createElement("div");
-    var editClassCol = createColBox.classList.add("col-3", "text-center", "bg-success","mb-5","ms-2","text-light",);
+    var editClassCol = createColBox.classList.add("col-3", "text-center", "bg-success","mb-5","ms-2","text-light");
     var pushToRow = clRow.appendChild(createColBox);
 
     var createImg = document.createElement("img");
-    createImg.setAttribute('src', Object.img)
+    createImg.setAttribute('src', i.img)
     createColBox.appendChild(createImg)
 
     var createName = document.createElement("h3");
-    createName.innerHTML = Object.name
+    createName.innerHTML = i.name
     createColBox.appendChild(createName)
 
     var createCandy = document.createElement("p");
-    createCandy.innerHTML = Object.candy;
+    createCandy.innerHTML = i.candy;
     createColBox.appendChild(createCandy)
 
     var createnum = document.createElement("p");
-    createnum.innerHTML = Object.num;
+    createnum.innerHTML = i.num;
     createColBox.appendChild(createnum)
 
     var createSpawnTime = document.createElement("p");
-    createSpawnTime.innerHTML = Object.spawn_time;
+    createSpawnTime.innerHTML = i.spawn_time;
     createColBox.appendChild(createSpawnTime)
   }
 }
